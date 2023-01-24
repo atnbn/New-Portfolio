d79ede12597a61d2dae69cfdb79233f3b53bdcb0
atnbn <86778371+atnbn@users.noreply.github.com>
2023-01-24 23:27:02 +0100

    add

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
-
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Renderer2 } from '@angular/core';
+import { ScrollService } from '../service/scroll.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-
+  constructor(public scrollService: ScrollService) {}
+  ngOnInit(): void {}
 }
---
+++ src/app/skill-stack/skill-stack.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-skill-stack',
+  templateUrl: './skill-stack.component.html',
+  styleUrls: ['./skill-stack.component.scss']
+})
+export class SkillStackComponent {
+
+}
